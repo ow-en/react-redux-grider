f9e4157dc2cee8765df9d2cd9dfcc5cb17d5a522
owensource <ciruz001@cougars.csusm.edu>
2018-03-27 15:00:22 -0700

    pushing files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAUx1WbrXruQtf8DN0LB3-iG09f3OQYxW4';
+
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
